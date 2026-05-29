5a4d83897d8300724667d42089a0af143865cb8b
Joe Zawisa <36611576+joezawisa@users.noreply.github.com>
2026-05-29 22:47:07 +0000

    Initial commit
    - Set up dev container
    - Create C# project
    - Add health check class
    - Add extension class

---
+++ AspNetCore.HealthChecks.Http/HealthChecksBuilderExtensions.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace AspNetCore.HealthChecks.Http
+{
+    /// <summary>
+    /// Provides basic extension methods for registering <see cref="HttpHealthCheck"/>s in an <see cref="IHealthChecksBuilder"/>.
+    /// </summary>
+    public static class HealthChecksBuilderExtensions
+    {
+        /// <summary>
+        /// Adds a new HTTP health check with the specified name and URL.
+        /// <para/>
+        /// <example>
+        /// This shows how to add an HTTP health check with no custom options.
+        /// <code>
+        /// healthChecksBuilder.AddHttpHealthCheck(name: "Example", url: "https://example.com/health");
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <param name="url">URL for the health check to request.</param>
+        /// <inheritdoc cref="HealthChecksBuilderAddCheckExtensions.AddTypeActivatedCheck" path="/param"/>
+        /// <inheritdoc cref="HealthChecksBuilderAddCheckExtensions.AddTypeActivatedCheck" path="/returns"/>
+        public static IHealthChecksBuilder AddHttpHealthCheck(
+            this IHealthChecksBuilder builder,
+            string name,
+            string url
+        ) => builder.AddTypeActivatedCheck<HttpHealthCheck>(
+            name: name,
+            args: url
+        );
+
+        /// <inheritdoc cref="AddHttpHealthCheck(IHealthChecksBuilder, string, string)"/>
+        /// <inheritdoc cref="HealthChecksBuilderAddCheckExtensions.AddTypeActivatedCheck(IHealthChecksBuilder, string, HealthStatus?, object[])" path="/param"/>
+        public static IHealthChecksBuilder AddHttpHealthCheck(
+            this IHealthChecksBuilder builder,
+            string name,
+            string url,
+            HealthStatus? failureStatus
+        ) => builder.AddTypeActivatedCheck<HttpHealthCheck>(
+            name: name,
+            failureStatus: failureStatus,
+            args: url
+        );
+
+        /// <inheritdoc cref="AddHttpHealthCheck(IHealthChecksBuilder, string, string, HealthStatus?)"/>
+        /// <inheritdoc cref="HealthChecksBuilderAddCheckExtensions.AddTypeActivatedCheck(IHealthChecksBuilder, string, HealthStatus?, IEnumerable&lt;string&gt;, object[])" path="/param"/>
+        public static IHealthChecksBuilder AddHttpHealthCheck(
+            this IHealthChecksBuilder builder,
+            string name,
+            string url,
+            HealthStatus? failureStatus,
+            IEnumerable<string> tags
+        ) => builder.AddTypeActivatedCheck<HttpHealthCheck>(
+            name: name,
+            failureStatus: failureStatus,
+            tags: tags,
+            args: url
+        );
+
+        /// <inheritdoc cref="AddHttpHealthCheck(IHealthChecksBuilder, string, string, HealthStatus?, IEnumerable&lt;string&gt;)"/>
+        /// <inheritdoc cref="HealthChecksBuilderAddCheckExtensions.AddTypeActivatedCheck(IHealthChecksBuilder, string, HealthStatus?, IEnumerable&lt;string&gt;, TimeSpan, object[])" path="/param"/>
+        public static IHealthChecksBuilder AddHttpHealthCheck(
+            this IHealthChecksBuilder builder,
+            string name,
+            string url,
+            HealthStatus? failureStatus,
+            IEnumerable<string> tags,
+            TimeSpan timeout
+        ) => builder.AddTypeActivatedCheck<HttpHealthCheck>(
+            name: name,
+            failureStatus: failureStatus,
+            tags: tags,
+            timeout: timeout,
+            args: url
+        );
+    }
+}
---
+++ AspNetCore.HealthChecks.Http/HttpHealthCheck.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace AspNetCore.HealthChecks.Http
+{
+    /// <summary>
+    /// A health check that makes a request to an HTTP endpoint.
+    /// </summary>
+    public class HttpHealthCheck : IHealthCheck {
+        private readonly HttpClient _httpClient;
+        private readonly string _url;
+
+        /// <summary>
+        /// Creates an <see cref="HttpHealthCheck"/>. Intended for dependency
+        /// injection.
+        /// </summary>
+        /// <param name="httpClientFactory">Factory to create an HTTP client.</param>
+        /// <param name="url">Full URL to check.</param>
+        public HttpHealthCheck(
+            IHttpClientFactory httpClientFactory,
+            string url
+        )
+        {
+            if (httpClientFactory is null) throw new ArgumentNullException(paramName: nameof(httpClientFactory));
+            if (url is null) throw new ArgumentNullException(paramName: nameof(url));
+
+            _httpClient = httpClientFactory.CreateClient();
+            _url = url;
+        }
+
+        /// <inheritdoc/>
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default
+        ) {
+            if (context is null) throw new ArgumentNullException(paramName: nameof(context));
+
+            try {
+                HttpResponseMessage response = await _httpClient.GetAsync(
+                    requestUri: _url,
+                    completionOption: HttpCompletionOption.ResponseHeadersRead,
+                    cancellationToken: cancellationToken
+                );
+
+                ReadOnlyDictionary<string, object> data = new ReadOnlyDictionary<string, object>(
+                    dictionary: new Dictionary<string, object>()
+                    {
+                        { nameof(response.StatusCode), response.StatusCode },
+                        { nameof(response.Content), response.Content },
+                        { nameof(response.Headers), response.Headers }
+                    }
+                );
+
+                if (response.IsSuccessStatusCode) {
+                    return HealthCheckResult.Healthy(
+                        description: $"Received a success response from {_url}.",
+                        data: data
+                    );
+                }
+                
+                return new HealthCheckResult(
+                    status: context.Registration.FailureStatus,
+                    description: $"Received a failure response from {_url}.",
+                    data: data
+                );
+            } catch (HttpRequestException exception) {
+                return new HealthCheckResult(
+                    status: context.Registration.FailureStatus,
+                    description: $"Did not receive a response from {_url}.",
+                    exception: exception
+                );
+            } catch (OperationCanceledException exception) when (cancellationToken.IsCancellationRequested)
+            {
+                return new HealthCheckResult(
+                    status: context.Registration.FailureStatus,
+                    description: $"Request to {_url} timed out.",
+                    exception: exception
+                );
+            }
+        }
+    }
+}
